873c124f717dfd6e74c4a8b43ab9086f8b75fa88
Surya Ramaraj <surya.ramaraj@happyreturns.com>
2025-05-14 16:44:25 -0400

    Clean up

--- chatbot/vite.config.ts
+++ chatbot/vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8002',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
