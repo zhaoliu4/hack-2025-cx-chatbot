c0ee78379dd1d01ce7fe4f96858e5c7f9aea6212
Chase Burgess <chase.burgess@happyreturns.com>
2025-05-14 18:02:12 -0400

    Add proxy for frontend req

--- chatbot/vite.config.ts
+++ chatbot/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/chat': {
+        target: 'http://localhost:8002',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/api': {
+        target: 'http://localhost:8002',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
